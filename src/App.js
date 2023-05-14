d0a8f3c2ecd236ad4c42adbb061c534ead4df6f6
platisam <platisam@gmail.com>
2023-05-14 18:44:30 +0200

    added state and style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [todoList, setTodoList] = useState([]);
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input />
+        <button>Add Task</button>
+      </div>
+      <div className="list"></div>
+    </div>
+  );
 }
 
 export default App;
