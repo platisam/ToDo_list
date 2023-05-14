318e1dc7df454b14515a580d0844c6456488ed06
platisam <platisam@gmail.com>
2023-05-14 21:13:34 +0200

    added onClick and onChange events

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import "./App.css";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+  };
+
   return (
     <div className="App">
       <div className="addTask">
-        <input />
-        <button>Add Task</button>
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
       </div>
       <div className="list"></div>
     </div>
